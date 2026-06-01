f993a5d6cc6721bf1bd7124d8efc62b09eb80c6a
Beto <betomoedano@outlook.com>
2026-06-01 10:03:35 -0500

    Wrap Stack in ThemeProvider to support dark and light themes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,21 @@
-import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
+  const theme = useColorScheme();
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen
-        name="new"
-        options={{
-          title: "New Task",
-          presentation: "modal",
-          headerLargeTitleEnabled: true,
-        }}
-      />
-    </Stack>
+    <ThemeProvider value={theme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen
+          name="new"
+          options={{
+            title: "New Task",
+            presentation: "modal",
+            headerLargeTitleEnabled: true,
+          }}
+        />
+      </Stack>
+    </ThemeProvider>
   );
 }
